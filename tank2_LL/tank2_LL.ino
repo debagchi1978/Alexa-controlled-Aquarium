76cd535e29fd7f9fbd3f4562293bcf817d9cbc89
Debajyoti Bagchi <debajyoti.bagchi@gmail.com>
2018-05-26 12:36:37 +0530

    tank 2 integrated with OTA update

--- tank2_LL/tank2_LL.ino
+++ tank2_LL/tank2_LL.ino
@@ -186,11 +186,11 @@ void setup() {
    ArduinoOTA.setHostname("tank2_LL");
 
   // No authentication by default
-   ArduinoOTA.setPassword("admin");
+   //ArduinoOTA.setPassword("admin");
 
   // Password can be set with it's md5 value as well
   // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-   ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+   //ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
 
   ArduinoOTA.onStart([]() {
     String type;
